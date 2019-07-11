aeae27f6d8a836fc85d070b61ad217d3de86f353
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-07-11 17:23:30 +0900

    feat: add convert UTM function

--- latlon.py
+++ latlon.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+from pyproj import Proj
+
 def MAKE_LATLON(degree, minutes, seconds):
     return float(degree) + float(minutes) / 60 + float(seconds) / 3600
 
 def CONVERT_WGS(fake_lat,fake_lon):
     lat = fake_lat - fake_lat * 0.00010695 + fake_lon * 0.000017464 + 0.0046017
     lon = fake_lon - fake_lat * 0.000046038 - fake_lon * 0.000083043 + 0.010040
-    return [lat,lon]
+    convertor = Proj(proj='utm', zone=54, ellps='WGS84')
+    x, y = convertor(lon, lat)
+    return [x,y]
