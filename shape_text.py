c61956df817bc7842b7f103f2c7d4bd1bbb3e0f1
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-07-11 16:29:54 +0900

    fix: regular expression

--- shape_text.py
+++ shape_text.py
@@ -4,12 +4,12 @@ from latlon import *
 
 def SHAPE_TEXT(text):
     time = re.search(r"[0-9]+:[0-9]+", text).group(0)
-    direction = re.search(r"([0-9]+).?\s*[一-龥]+", text).group(1)
+    direction = re.search(r"([0-9]+)[。°]?\s*[北南東西]{1,2}", text).group(1)
     # ido
-    lat_text = re.search(r"北緯*\s*(\d{2}).?(\d{1,2})\'(\d{1,2})\"", text)
+    lat_text = re.search(r"北緯?\s*(\d{2}).?(\d{1})\'?(\d{1,2})\"", text)
     fake_lat = MAKE_LATLON(lat_text.group(1), lat_text.group(2), lat_text.group(3))
     # keido
-    lon_text = re.search(r"東経*\s*(\d{3}).?(\d{1,2})\'(\d{1,2})\"", text)
+    lon_text = re.search(r"東経?\s*(\d{3}).?(\d{1})\'?(\d{1,2})\"", text)
     fake_lon = MAKE_LATLON(lon_text.group(1), lon_text.group(2), lon_text.group(3))
     lat = CONVERT_WGS(fake_lat,fake_lon)[0]
     lon = CONVERT_WGS(fake_lat,fake_lon)[1]
