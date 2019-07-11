61fb9f276faee628651c3fc49da44bb8e4cbe4c6
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-07-11 13:59:27 +0900

    feat: add input functiuon for exception

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import glob
 import sys
 import pandas as pd
+from PIL import Image
 from ocr import *
 from shape_text import *
 
@@ -14,8 +16,17 @@ def MAKE_FILES(date):
         try:
             arr.append(SHAPE_TEXT(text))
         except:
-            print(image)
-            pass
+            img = Image.open(image)
+            img.show()
+            lat_d,lat_m,lat_s = input('Enter degree, minutes and seconds of latitude(ido).\n').split()
+            lon_d,lon_m,lon_s = input('Enter degree, minutes and seconds of longitude(keido).\n').split()
+            time = re.search(r"[0-9]+:[0-9]+", text).group(0)
+            direction = re.search(r"([0-9]+).?\s*[一-龥]+", text).group(1)
+            fake_lat = MAKE_LATLON(lat_d, lat_m, lat_s)
+            fake_lon = MAKE_LATLON(lon_d, lon_m, lon_s)
+            lat = CONVERT_WGS(fake_lat,fake_lon)[0]
+            lon = CONVERT_WGS(fake_lat,fake_lon)[1]
+            arr.append([time, direction, lat, lon])
     name = './data/' + date + '.csv'
     df = pd.DataFrame(arr, columns=['time', 'direction', 'lat', 'lon'])
     df.to_csv(name, header=True, index=False)
