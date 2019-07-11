1ba3818d8ce44221418ead55716319b887666763
Yuto NAKAIZUMI <yutonakaizumi@YutonoMacBook-Pro.local>
2019-07-11 17:24:05 +0900

    add

--- shape_text.py
+++ shape_text.py
@@ -4,7 +4,7 @@ from latlon import *
 
 def SHAPE_TEXT(text):
     time = re.search(r"[0-9]+:[0-9]+", text).group(0)
-    direction = re.search(r"([0-9]+)[。°]?\s*[北南東西]{1,2}", text).group(1)
+    direction = re.search(r"([0-9]+)[。°]+\s*[北南東西]{1,2}", text).group(1)
     # ido
     lat_text = re.search(r"北緯?\s*(\d{2}).?(\d{1})\'?(\d{1,2})\"", text)
     fake_lat = MAKE_LATLON(lat_text.group(1), lat_text.group(2), lat_text.group(3))
